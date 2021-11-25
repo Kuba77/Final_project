f56c4eb23d428c8a879eb27e39d10c452fa014c4
Arkadii <user@MacBook-Air-Arkadii.local>
2021-11-25 17:24:07 +0200

    add main navbar and header componets

---
+++ client/src/components/Header/Nav/Nav.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import classes from "./Nav.module.scss";
+
+
+const Nav = ({ isMenu, menuToggle }) => {
+    return (
+        <nav className={isMenu ? classes.menu__nav : classes.nav}>
+            <ul>
+            <li onClick={menuToggle}>
+                    <Link to="/">Home</Link>
+                </li>
+                
+                {isMenu ? (
+                    <li onClick={menuToggle}>
+                        <Link to="/login">Sign in</Link>
+                    </li>
+                ): null}
+               
+                <li onClick={menuToggle}>
+                    <Link to="/categories">Categories</Link>
+                </li>
+                <li onClick={menuToggle}>
+                    <Link to="/favorites">Favorites</Link>
+                </li>
+            </ul>
+          
+        </nav>
+    );
+};
+
+export default Nav;
