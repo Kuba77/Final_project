eb95dd410b3111b2a49de6d2154d952bce35af7e
Kuba77 <optimistic376@gmail.com>
2021-11-30 20:16:02 +0200

    fixed app

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,14 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header/Header";
-import formStyle from "../src/styles/form/form.scss"
-import RegistrationForm from "./components/form/RegistrationForm";
+
 
 
 function App() {
   return (
     <Provider store={store}>
-      <Router>
+       <Router>
         <Header />
-      </Router>
-      <Router>
-      <div className="app">
-   <RegistrationForm />
-    </div>
       </Router>
     </Provider>
  
