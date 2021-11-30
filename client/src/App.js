be721e07e12ac9fea595ba9024daeffda40b123b
Arkadii <user@MacBook-Air-Arkadii.local>
2021-11-30 18:19:28 +0200

    add Counter componet to basket

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { BrowserRouter as Router } from "react-router-dom";
-
-
+import Header from '../src/components/Header/Header'
+import Main from '../src/components/Main/Main'
 
 function App() {
   return (
     <Provider store={store}>
       <Router>
-      
+          <Header />
+          <Main />   
       </Router>
 
     </Provider>
--- client/src/components/Header/Basket/Basket.js
+++ client/src/components/Header/Basket/Basket.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { BsBasket } from 'react-icons/bs';
+import Counter from './Counter/Counter'
 import classes from './Basket.module.scss'
 
 
 const Basket = () => {
+
     return (
         <div className={classes.header__cart}>
+            <Counter quantity={2}/>
+
             <Link to="/cart">
                  <BsBasket  color='black' size={22}/>
             </Link>
