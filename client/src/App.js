13375c109c6371f614447018d8b7996fba697ef2
Kuba77 <optimistic376@gmail.com>
2021-12-02 16:26:47 +0200

    workin with form

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,22 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header/Header";
+import SighnUpForm from "./components/form/SighnUpForm";
+import stylesForm from "../src/styles/form/form.scss"
 
 
 
 function App() {
   return (
-    <Provider store={store}>
-       <Router>
-        <Header />
-      </Router>
-    </Provider>
+    // <Provider store={store}>
+    //    <Router>
+    //     <Header />
+    //   </Router>
+      <div className="app">
+       <SighnUpForm />
+      </div>
+    // </Provider>
+   
  
   );
 }
