fd66774622e35f9b335c7906d6ae744532740e9f
Arkadii <user@MacBook-Air-Arkadii.local>
2021-12-05 20:40:53 +0200

    fix slider for moblie view

--- client/src/components/Feature/Feature.js
+++ client/src/components/Feature/Feature.js
@@ -5,19 +5,19 @@ import Carousel from './Carousel/Carousel'
 import useWindowSize from '../../hooks/useWindowSize'
 
 const Feature = () => {
-    const size = useWindowSize()
+  
 
     return (
         <section className={classes.feature}>
             <div className={layout.section}>
          
                 <div className={classes.feature__container}>
-                    {size.width > 768 && (
+           
                         <>
                             <h2>Featured Manga</h2>
                             <Carousel/>
                         </>
-                    )} 
+                
             </div>
             </div>
         </section>
