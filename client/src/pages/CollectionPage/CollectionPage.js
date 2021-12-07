24ada402e028bfa59cb1b507681b1a12f762cd25
Misyats-Denis <misyatsdenis@gmail.com>
2021-12-07 19:09:10 +0200

    fix basket counter/ customer name add in button

--- client/src/pages/CollectionPage/CollectionPage.js
+++ client/src/pages/CollectionPage/CollectionPage.js
@@ -1,64 +1,88 @@
-import React, {useEffect, useState} from 'react';
-import Header from '../../components/Header/Header';
-import classes from './CollectionPage.module.scss';
-import PuffLoader from 'react-spinners/PuffLoader';
+import React, { useEffect, useState, useCallback } from "react";
+import Header from "../../components/Header/Header";
+import classes from "./CollectionPage.module.scss";
+import PuffLoader from "react-spinners/PuffLoader";
 import { getAllProducts } from "../../api/productsApi";
-import CollectionList from './CollectionList/CollectionList';
-import Pagination from './Pagination/Pagination'
+import CollectionList from "./CollectionList/CollectionList";
+import Pagination from "./Pagination/Pagination";
 
+import Filters from "../../components/Filters/Filters";
+import { chekingArray } from "../../utils/utils";
+import { getFilteredProductByQuery } from "../../api/productsApi";
 
 const CollectionPage = () => {
+  const [collection, setCollection] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsInPage] = useState(15);
 
-    const [collection, setCollection] = useState([]);
-    const [isLoading, setLoading] = useState(false);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [productsInPage] = useState(15);
+  const [genderSelected, setgenderSelected] = useState([]);
 
-    useEffect(() => {
-        const getCollection = async () => {
-          setLoading(true);
-          const response = await getAllProducts();
-          console.log(response);
-          setCollection(response);
-          setLoading(false);
-        };
-        getCollection();
-      }, []);
+  function getselectedGenre(value) {
+    const selected = chekingArray(genderSelected, value);
+    console.log(selected);
+    setgenderSelected(selected);
+  }
 
-    const lastProductIndex = currentPage * productsInPage;
-    const firstProductIndex = lastProductIndex - productsInPage;
-    const currentProduct = collection.slice(firstProductIndex, lastProductIndex);
+  const getGenderProducts = useCallback(
+    async (value) => {
+      let params = new URLSearchParams();
+      params.append("genre", value);
+      let string = params.toString();
+      const products = await getFilteredProductByQuery(string);
+      setCollection(products.products);
+    },
+    [setCollection]
+  );
 
-    const paginate = (pageNumber, setLoading) => {
+  useEffect(() => {
+    let GenString = genderSelected.join();
+    getGenderProducts(GenString);
+  }, [genderSelected]);
 
-        setCurrentPage(pageNumber);
-       
-      };
+  useEffect(() => {
+    const getCollection = async () => {
+      setLoading(true);
+      const response = await getAllProducts();
+      console.log(response);
+      setCollection(response);
+      setLoading(false);
+    };
+    getCollection();
+  }, []);
 
-    return (
-        <>
-            <Header />
-            <section className={classes.collection}>
-            {isLoading && (
-                    <div className={classes.collection__loader}>
-                        <PuffLoader loading={isLoading} color='purple' size={120}/>
-                    </div>
-                )}
-            <div className={classes.collection__container}>
-            {!isLoading && (
-                <CollectionList collection={currentProduct}/> 
-                )}
-            </div>
+  const lastProductIndex = currentPage * productsInPage;
+  const firstProductIndex = lastProductIndex - productsInPage;
+  const currentProduct = collection.slice(firstProductIndex, lastProductIndex);
 
-            <Pagination
-                productsInPage={productsInPage}
-                totalProducts={collection.length}
-                paginate={paginate}
-            />
-          
-        </section>
-        </>
-    )
-}
+  const paginate = (pageNumber, setLoading) => {
+    setCurrentPage(pageNumber);
+  };
 
-export default CollectionPage
+  return (
+    <>
+      <Header />
+      <section className={classes.collection}>
+        {isLoading && (
+          <div className={classes.collection__loader}>
+            <PuffLoader loading={isLoading} color="purple" size={120} />
+          </div>
+        )}
+
+        <Filters onChange={getselectedGenre} />
+
+        <div className={classes.collection__container}>
+          {!isLoading && <CollectionList collection={currentProduct} />}
+        </div>
+
+        <Pagination
+          productsInPage={productsInPage}
+          totalProducts={collection.length}
+          paginate={paginate}
+        />
+      </section>
+    </>
+  );
+};
+
+export default CollectionPage;
