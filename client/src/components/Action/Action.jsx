8f1220e783a501f6885ecb2bfb42201968b171c7
FIRST_NAME LAST_NAMEFIRST_NAME <baranetskaya.alina@gmail.com>
2021-12-22 05:42:07 +0200

    change action block

--- client/src/components/Action/Action.jsx
+++ client/src/components/Action/Action.jsx
@@ -1,24 +1,17 @@
 import React, { useEffect, useState, useCallback } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { useParams, Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { getAllProducts } from "../../services/products";
-import { BsBasket, BsCaretLeftFill, BsCaretRightFill } from "react-icons/bs";
-import { setItemInCart, deleteItemFromCart } from "../../store/cart/reducer";
-import ProductTitle from "../Product/ProductTitle/ProductTitle";
-import ProductAuthor from "../Product/ProductAuthor/ProductAuthor";
-import ProductPrice from "../Product/ProductPrice/ProductPrice";
-import ProductImg from "../Product/ProductImg/ProductImg";
-import ProductStore from "../Product/ProductStore/ProductStore";
 import classes from "./Action.module.scss";
-import Button from "../Button/Button";
 import Timer from "../Timer/Timer";
 import PropTypes from "prop-types";
+import ActionCard from "./ActionCard/ActionCard";
+import SwiperCore, { Navigation, Autoplay } from "swiper/core";
+
+SwiperCore.use([Navigation, Autoplay]);
 
 const Action = (props) => {
   const { timer } = props;
   const [action, setAction] = useState([]);
-  const [firstCardToggle, setFirstCardToggle] = useState(0);
-  const [secondCardToggle, setSecondCardToggle] = useState(1);
   const [actionTime, setActionTime] = useState();
 
   const getCollection = useCallback(async () => {
@@ -35,35 +28,7 @@ const Action = (props) => {
 
   useEffect(() => {
     getCollection();
-  }, []);
-
-  const rightClick = useCallback(() => {
-    if (firstCardToggle === action.length - 1) {
-      setFirstCardToggle(0)
-    } else {
-      setFirstCardToggle(firstCardToggle + 1)
-    }
-
-    if (secondCardToggle === action.length - 1) {
-      setSecondCardToggle(0)
-    } else {
-      setSecondCardToggle(secondCardToggle + 1)
-    }
-  }, [firstCardToggle, secondCardToggle, action.length])
-
-  const leftClick = useCallback(() => {
-    if (firstCardToggle === 0) {
-      setFirstCardToggle(action.length - 1)
-    } else {
-      setFirstCardToggle(firstCardToggle - 1)
-    }
-
-    if (secondCardToggle === 0) {
-      setSecondCardToggle(action.length - 1)
-    } else {
-      setSecondCardToggle(secondCardToggle - 1)
-    }
-  }, [firstCardToggle, secondCardToggle, action.length])
+  });
 
   const getActionTime = useCallback(() => {
     if (new Date(timer).getTime() > new Date().getTime()) {
@@ -77,9 +42,10 @@ const Action = (props) => {
 
   useEffect(() => {
     getActionTime();
-  }, []);
+  });
 
   return (
+
     <React.Fragment>
       {action.length !== 0 && actionTime ?
         (<div className={classes.action_block}>
@@ -91,135 +57,26 @@ const Action = (props) => {
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
               <Timer time={timer} />
             </div>
-
             <div className={classes.action_content}>
+            <Swiper
+              navigation={true}
+              loop={true}
+              pagination={{
+                clickable: true,
+              }}
+            >
 
-              <Button
-                type="btn--secondary"
-                onClick={() => { leftClick() }}
-              >
-                <BsCaretLeftFill color="#8D28AD" size={26} />
-              </Button>
+                {action.map((item, index) => (
+                  <SwiperSlide key={index}>
+                    <ActionCard
+                      key={item.name}
+                      item={item}
+                    />                   
+                  </SwiperSlide>
+                ))}
 
-              <div className={classes.action}>
-
-                <ProductImg
-                  className={classes.action_img}
-                  item={action[firstCardToggle].imageUrls[0]}
-                  alt={action[firstCardToggle].name}
-                />
-
-                <div className={classes.action_info}>
-
-                  <Link to={`/product/${action[firstCardToggle].itemNo}`}>
-                    <ProductTitle
-                      className={classes.action_info__title}
-                      title={action[firstCardToggle].name}
-                    />
-                  </Link>
-
-                  <ProductAuthor
-                    className={classes.action_info__author}
-                    author={action[firstCardToggle].author}
-                  />
-
-                  <div className={classes.action_info__priceblock}>
-                    <ProductPrice
-                      className={classes.action_info__price}
-                      price={action[firstCardToggle].salePrice}
-                    />
-
-                    <ProductPrice
-                      className={classes.action_info__price}
-                      price={action[firstCardToggle].currentPrice}
-                    />
-                  </div>
-
-                  {action[firstCardToggle].quantity > 0 ? (
-                    <ProductStore
-                      className={classes.product_info__store}
-                      store={`In stock ${action[firstCardToggle].quantity}`}
-                    />
-                  ) : (
-                    <ProductStore
-                      className={classes.product_info__store}
-                      store="Sold out"
-                    />
-                  )}
-
-                  {/* <Button
-                    type="main"
-                    }}
-                  >
-                    <BsBasket color="white" size={26}
-                    />
-                  </Button> */}
-
-                </div>
-              </div>
-
-              <Button
-                type="btn--secondary"
-                onClick={() => { rightClick() }}
-              >
-                <BsCaretRightFill color="#8D28AD" size={26} />
-              </Button>
-
-              {action.length >= 2 ?
-                (
-                  <div className={classes.action}>
-                    <ProductImg
-                      className={classes.action_img}
-                      item={action[secondCardToggle].imageUrls[0]}
-                      alt={action[secondCardToggle].name}
-                    />
-
-                    <div className={classes.action_info}>
-                      <ProductTitle
-                        className={classes.action_info__title}
-                        title={action[secondCardToggle].name}
-                      />
-
-                      <ProductAuthor
-                        className={classes.action_info__author}
-                        author={action[secondCardToggle].author}
-                      />
-
-                      <div className={classes.action_info__priceblock}>
-                        <ProductPrice
-                          className={classes.action_info__price}
-                          price={action[secondCardToggle].salePrice}
-                        />
-
-                        <ProductPrice
-                          className={classes.action_info__price}
-                          price={action[secondCardToggle].currentPrice}
-                        />
-                      </div>
-
-                      {action[secondCardToggle].quantity > 0 ? (
-                        <ProductStore
-                          className={classes.product_info__store}
-                          store={`In stock ${action[secondCardToggle].quantity}`}
-                        />
-                      ) : (
-                        <ProductStore
-                          className={classes.product_info__store}
-                          store="Sold out"
-                        />
-                      )}
-
-                      {/* <Button
-                        type="main"
-                      >
-                        <BsBasket color="white" size={26} />
-                      </Button> */}
-                    </div>
-                  </div>
-
-                ) : ("")}
-
-            </div>
+            </Swiper>
+            </div>           
           </div>
 
           <div className={classes.blur}></div>
--- client/src/components/Action/ActionCard/ActionCard.jsx
+++ client/src/components/Action/ActionCard/ActionCard.jsx
@@ -1,79 +1,72 @@
-import React, { useEffect, useState, useCallback } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { useParams, Link } from "react-router-dom";
-import ProductTitle from "../Product/ProductTitle/ProductTitle";
-import ProductAuthor from "../Product/ProductAuthor/ProductAuthor";
-import ProductPrice from "../Product/ProductPrice/ProductPrice";
-import ProductImg from "../Product/ProductImg/ProductImg";
-import ProductStore from "../Product/ProductStore/ProductStore";
+import React from "react";
+import { Link } from "react-router-dom";
+import ProductTitle from "../../Product/ProductTitle/ProductTitle";
+import ProductAuthor from "../../Product/ProductAuthor/ProductAuthor";
+import ProductPrice from "../../Product/ProductPrice/ProductPrice";
+import ProductImg from "../../Product/ProductImg/ProductImg";
+import ProductStore from "../../Product/ProductStore/ProductStore";
 import classes from "./ActionCard.module.scss";
-import Button from "../Button/Button";
 import PropTypes from "prop-types";
 
 const ActionCard = (props) => {
-    
+    const { item } = props
     return (
         <React.Fragment>
+            <Link to={`/product/${item.itemNo}`}>
+                <div className={classes.action}>
 
-            <div className={classes.action}>
-
-                <ProductImg
-                    className={classes.action_img}
-                    item={item.imageUrls[0]}
-                    alt={action[firstCardToggle].name}
-                />
-
-                <div className={classes.action_info}>
-
-                    <Link to={`/product/${action[firstCardToggle].itemNo}`}>
-                        <ProductTitle
-                            className={classes.action_info__title}
-                            title={action[firstCardToggle].name}
-                        />
-                    </Link>
-
-                    <ProductAuthor
-                        className={classes.action_info__author}
-                        author={action[firstCardToggle].author}
+                    <ProductImg
+                        className={classes.action_img}
+                        item={item.imageUrls[0]}
+                        alt={item.name}
                     />
 
-                    <div className={classes.action_info__priceblock}>
-                        <ProductPrice
-                            className={classes.action_info__price}
-                            price={action[firstCardToggle].salePrice}
+                    <div className={classes.action_info}>
+
+                        <ProductTitle
+                            className={classes.action_info__title}
+                            title={item.name}
                         />
 
-                        <ProductPrice
-                            className={classes.action_info__price}
-                            price={action[firstCardToggle].currentPrice}
+                        <ProductAuthor
+                            className={classes.action_info__author}
+                            author={item.author}
                         />
+
+                        <div className={classes.action_info__priceblock}>
+                            <ProductPrice
+                                className={classes.action_info__price}
+                                price={item.salePrice}
+                            />
+
+                            <ProductPrice
+                                className={classes.action_info__price}
+                                price={item.currentPrice}
+                            />
+                        </div>
+
+                        {item.quantity > 0 ? (
+                            <ProductStore
+                                className={classes.product_info__store}
+                                store={`In stock ${item.quantity}`}
+                            />
+                        ) : (
+                            <ProductStore
+                                className={classes.product_info__store}
+                                store="Sold out"
+                            />
+                        )}
+
                     </div>
-
-                    {action[firstCardToggle].quantity > 0 ? (
-                        <ProductStore
-                            className={classes.product_info__store}
-                            store={`In stock ${action[firstCardToggle].quantity}`}
-                        />
-                    ) : (
-                        <ProductStore
-                            className={classes.product_info__store}
-                            store="Sold out"
-                        />
-                    )}
-
-                    <Button
-                        type="main"
-                        onClick={() => {
-                            console.log(action[firstCardToggle].productId);
-                        }}
-                    >
-                        <BsBasket color="white" size={26}
-                        />
-                    </Button>
-
                 </div>
-            </div>
-
+            </Link>
         </React.Fragment>
     );
 };
+
+ActionCard.propTypes = {
+    className: PropTypes.string,
+    description: PropTypes.string,
+};
+
+export default ActionCard;
