9cf595fd9f4727420a0ece11bb6162ddd312d50a
FIRST_NAME LAST_NAMEFIRST_NAME <baranetskaya.alina@gmail.com>
2022-01-12 16:35:23 +0200

    final fix

--- client/src/pages/FavoritesPage/FavoritesPage.js
+++ client/src/pages/FavoritesPage/FavoritesPage.js
@@ -3,37 +3,31 @@ import { useSelector } from "react-redux";
 import classes from "./FavoritesPage.module.scss";
 import FavoritesItem from "./FavoriteItem/FavoriteItem";
 import { itemsInFavorite } from "../../store/selectors";
+import Empty from "../../components/Empty/Empty";
 
 const FavoritesPage = () => {
   const store = useSelector((state) => state);
+  const favoritesItemsList = itemsInFavorite(store).map((item, index) => (
+    <FavoritesItem
+      key={index}
+      imgSrc={item.imageUrls[2]}
+      title={item.name}
+      author={item.author}
+      salePrice={item.salePrice}
+      price={item.currentPrice}
+      quantity={item.quantity}
+      itemNo={item.itemNo}
+      id={item._id}
+      item={item}
+    />
+  ));
 
   return (
     <>
       <section className={classes.favorites}>
         <h3 className={classes.favorites__title}>Your Wishlist</h3>
-        {itemsInFavorite(store).length === 0 && (
-          <>
-            <div className={classes.favorites__noitem}>
-              <img src="https://res.cloudinary.com/dl7xlw7cl/image/upload/v1639408051/sideAssets/SeekPng.com_anime-blush-png_380918_jzwoqn.png" />
-              <h3>No favorite Items</h3>
-            </div>
-          </>
-        )}
-        <div className={classes.favorites__wrapper}>
-          {itemsInFavorite(store).map((item, index) => (
-            <FavoritesItem
-              key={index}
-              imgSrc={item.imageUrls[2]}
-              title={item.name}
-              author={item.author}
-              salePrice={item.salePrice}
-              price={item.currentPrice}
-              quantity={item.quantity}
-              itemNo={item.itemNo}
-              item={item}
-            />
-          ))}
-        </div>
+        {itemsInFavorite(store).length === 0 && <Empty />}
+        <div className={classes.favorites__wrapper}>{favoritesItemsList}</div>
       </section>
     </>
   );
